7f764f5b236ce8e7faacb99b5dcb60126882002f
Henning Koch <henning.koch@makandra.de>
2016-02-22 12:09:50 +0100

    Remove deprecation warnings in new RSpecs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,13 @@
 
 $: << File.join(File.dirname(__FILE__), "/../../lib" )
 require 'cucumber_priority'
+
+if defined?(RSpec)
+  RSpec.configure do |config|
+    if config.respond_to?(:expect_with)
+      config.expect_with(:rspec) do |c|
+        c.syntax = [:expect, :should]
+      end
+    end
+  end
+end
