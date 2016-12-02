2d37e4e0ca12904fa68ee08c01440818225a3887
Mathieu Jobin <mathieu@venuenext.com>
2016-12-02 14:16:47 -0500

    use Module#prepend here as well

--- lib/cucumber_priority/ambiguous_error_ext.rb
+++ lib/cucumber_priority/ambiguous_error_ext.rb
@@ -1,11 +1,10 @@
-module Cucumber
-  class Ambiguous
+module CucumberAmbiguousWithMatches
+  attr_reader :matches
 
-    attr_reader :matches
-
-    def initialize(step_name, matches, *args)
-      @matches = matches
-      super(step_name, matches, *args)
-    end
+  def initialize(step_name, matches, *args)
+    @matches = matches
+    super(step_name, matches, *args)
   end
 end
+
+Cucumber::Ambiguous.prepend CucumberAmbiguousWithMatches
