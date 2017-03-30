35cca73a1282b73a925a8ab17c21901455f4e136
Igor Melnikov <turistpro@gmail.com>
2017-03-30 23:16:41 +0300

    remove exception

--- lib/cucumber_priority.rb
+++ lib/cucumber_priority.rb
@@ -2,7 +2,6 @@ require 'cucumber'
 require 'cucumber/rb_support/rb_language'
 
 require 'cucumber_priority/util'
-require 'cucumber_priority/ambiguous_error_ext'
 require 'cucumber_priority/rb_step_definition_ext'
 require 'cucumber_priority/rb_language'
 
