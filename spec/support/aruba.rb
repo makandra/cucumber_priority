e4ef7bed21c0912d6da6de2edd65ea633d0384fa
Michael Leimstaedtner <michael.leimstaedtner@makandra.de>
2025-01-20 17:27:33 +0100

    Add version switch for older cucumbers

--- spec/support/aruba.rb
+++ spec/support/aruba.rb
@@ -1,1 +1,21 @@
-require 'aruba/rspec'
+require 'aruba/version'
+
+if Gem::Version.new(Aruba::VERSION) >= Gem::Version.new('1.0.0')
+  require 'aruba/rspec'
+else
+  require 'aruba/api'
+  require 'aruba/in_process'
+  require 'cucumber/cli/main'
+
+  Aruba.process = Aruba::InProcess
+  Aruba::InProcess.main_class = Cucumber::Cli::Main
+
+  Gemika::RSpec.configure do |config|
+    config.include Aruba::Api
+
+    config.before(:each) do
+      restore_env
+      clean_current_dir
+    end
+  end
+end
